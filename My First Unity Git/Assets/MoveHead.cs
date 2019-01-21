47b5a9bbdd6c79164845b9b7a7f5c451540c7629
GrisWoldDiablo <griswoldd@hotmail.com>
2019-01-21 15:04:46 -0500

    Mouse Look script

--- My First Unity Git/Assets/MoveHead.cs
+++ My First Unity Git/Assets/MoveHead.cs
@@ -12,6 +12,7 @@ public class Limit
 public class MoveHead : MonoBehaviour
 {
     [SerializeField] private Limit limit;
+    [SerializeField] private Vector3 currentEuler;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +25,8 @@ public class MoveHead : MonoBehaviour
         float xDir = Input.GetAxis("Mouse X");
         float yDir = Input.GetAxis("Mouse Y");
         transform.Rotate(-yDir, xDir, 0.0f);
-
-        if (transform.rotation.eulerAngles.x < limit.top & transform.rotation.eulerAngles.x > -90.0f)
+        currentEuler = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        if (transform.rotation.eulerAngles.x < 360+limit.top && transform.rotation.eulerAngles.x > 360 + -90.0f)
         {
             transform.rotation = Quaternion.Euler(limit.top, transform.eulerAngles.y, transform.eulerAngles.z);
         }
