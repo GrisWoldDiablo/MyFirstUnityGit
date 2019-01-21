6ef740cf123e92d65aa402e61cb38f260e296ac4
GrisWoldDiablo <griswoldd@hotmail.com>
2019-01-21 15:41:07 -0500

    Added Move to the Lookscript and some Prefabs

--- My First Unity Git/Assets/MoveHead.cs
+++ My First Unity Git/Assets/MoveHead.cs
@@ -13,20 +13,23 @@ public class MoveHead : MonoBehaviour
 {
     [SerializeField] private Limit limit;
     [SerializeField] private Vector3 currentEuler;
+    [SerializeField] private float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
         float xDir = Input.GetAxis("Mouse X");
         float yDir = Input.GetAxis("Mouse Y");
         transform.Rotate(-yDir, xDir, 0.0f);
         currentEuler = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-        if (transform.rotation.eulerAngles.x < 360+limit.top && transform.rotation.eulerAngles.x > 360 + -90.0f)
+        if (transform.rotation.eulerAngles.x < 360 + limit.top && transform.rotation.eulerAngles.x > 360 + -90.0f)
         {
             transform.rotation = Quaternion.Euler(limit.top, transform.eulerAngles.y, transform.eulerAngles.z);
         }
@@ -36,5 +39,14 @@ public class MoveHead : MonoBehaviour
         }
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0.0f);
 
+        if (moveY != 0)
+        {
+            transform.position = transform.position + (transform.forward * moveY * Time.deltaTime * speed);
+        }
+
+        if (moveX != 0)
+        {
+            transform.position = transform.position + (transform.right * moveX * Time.deltaTime * speed); 
+        }
     }
 }
